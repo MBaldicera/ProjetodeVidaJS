152b7629baff702c6075678d7f9533b7d9d899ce
MBaldicera <126473999+MBaldicera@users.noreply.github.com>
2024-04-29 18:45:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,29 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2024-10-05T23:59:59");
+const tempoObjetivo2 = new Date("2024-12-05T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-30T23:59:59");
+const tempoObjetivo4 = new Date("2025-02-01T23:59:59");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+for(let i = 0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
